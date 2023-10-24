d02ceea14cc2d0cc5dd734949331c650b9ec1d29
marianoEG <marianen@live.com.ar>
2023-10-24 02:42:31 -0300

    Pagina reset password

--- src/services/reset_services.js
+++ src/services/reset_services.js
@@ -34,5 +34,5 @@ export async function updatePassword(dni, newPassword, hash) {
         }),
         referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     });
-    return response.json();
+    return response.status;
 }
