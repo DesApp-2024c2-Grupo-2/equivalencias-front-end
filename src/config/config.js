fe47167e5d69b8eac08518e4734785f1f41ea40e
stkener <stkener@gmail.com>
2024-11-07 14:20:49 -0300

    cambien apiUrl en config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 export const config = {
-    apiUrl: 'http://localhost:3001/api'
-    // apiUrl: 'https://unahur-equivalencias-backend.herokuapp.com/api'
+    //apiUrl: 'http://localhost:3001/api'
+    //apiUrl: 'https://unahur-equivalencias-backend.herokuapp.com/api'
+    apiUrl: 'https://equivalencias-back-end-2c2024.onrender.com/api'
 };
