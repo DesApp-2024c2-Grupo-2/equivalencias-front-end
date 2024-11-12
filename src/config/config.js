b5f3f209d2251f64f0d9082544d95b07142351de
stkener <stkener@gmail.com>
2024-11-12 00:07:45 -0300

    modifique config.js y app.jsx

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const config = {
     //apiUrl: 'http://localhost:3001/api'
     //apiUrl: 'https://unahur-equivalencias-backend.herokuapp.com/api'
-    apiUrl: 'https://equivalencias-back-end-2c2024.onrender.com/api'
+    apiUrl: 'wws://equivalencias-back-end-2c2024.onrender.com/api'
 };
